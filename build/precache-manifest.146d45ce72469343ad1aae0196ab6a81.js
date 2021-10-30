aad6b6ffd7256ed097c7b0522b47f729cbf2670b
dilan <dilan.roessell@gmail.com>
2021-10-30 18:15:54 -0400

    added project dependencies to our package.json

---
+++ build/precache-manifest.146d45ce72469343ad1aae0196ab6a81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6ec5302d838c376fa7b",
+    "url": "/cyanlul/COMP3104/static/css/main.1690b1fd.chunk.css"
+  },
+  {
+    "revision": "f6ec5302d838c376fa7b",
+    "url": "/cyanlul/COMP3104/static/js/main.f6ec5302.chunk.js"
+  },
+  {
+    "revision": "b7b3f174baeba97daafd",
+    "url": "/cyanlul/COMP3104/static/js/runtime~main.b7b3f174.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/cyanlul/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "945c89a040a09572b73902a3b5b95f2c",
+    "url": "/cyanlul/COMP3104/index.html"
+  }
+];

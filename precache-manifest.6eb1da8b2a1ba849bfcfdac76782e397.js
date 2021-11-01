e216e35d4bf77f7906fb3a3312eece15fbe114c3
Dilan <91137926+cyanlul@users.noreply.github.com>
2021-10-31 21:34:10 -0400

    Add files via upload

---
+++ precache-manifest.6eb1da8b2a1ba849bfcfdac76782e397.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dac8ae5fa03f3429b2bb",
+    "url": "/Lhosel/COMP3104/static/css/main.46566c1f.chunk.css"
+  },
+  {
+    "revision": "dac8ae5fa03f3429b2bb",
+    "url": "/Lhosel/COMP3104/static/js/main.dac8ae5f.chunk.js"
+  },
+  {
+    "revision": "5d4b8e170f76f3edadf7",
+    "url": "/Lhosel/COMP3104/static/js/runtime~main.5d4b8e17.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Lhosel/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "3b2497fbafd1763d4743e399accc74f6",
+    "url": "/Lhosel/COMP3104/index.html"
+  }
+];
